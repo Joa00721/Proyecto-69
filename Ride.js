f56bb1bbf88dc1380634e1a5ce9d8747a23d24e9
Joa00721 <86857876+Joa00721@users.noreply.github.com>
2022-02-05 21:22:18 -0600

    Add files via upload

---
+++ Ride.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import * as Permissions from "expo-permissions";
+import { BarCodeScanner } from "expo-barcode-scanner";
+
+export default class TransactionScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      domState: "normal",
+      hasCameraPermissions: null,
+      scanned: false,
+      scannedData: ""
+    };
+  }
+
+  getCameraPermissions = async domState => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+    this.setState({
+      /*status === "granted" es verdadero cuando el usuario ha dado permiso
+          status === "granted" es falso cuando el usuario no ha dado permiso 
+        */
+      hasCameraPermissions: status === "granted",
+      domState: domState,
+      scanned: false
+    });
+  };
+
+  handleBarCodeScanned = async ({ type, data }) => {
+    this.setState({
+      scannedData: data,
+      domState: "normal",
+      scanned: true
+    });
+  };
+
+  render() {
+    const { domState, hasCameraPermissions, scannedData, scanned } = this.state;
+    if (domState === "scanner") {
+      return (
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          {hasCameraPermissions ? scannedData : "Solicitar permiso de la cámara"}
+        </Text>
+        <TouchableOpacity
+          style={[styles.button, { marginTop: 25 }]}
+          onPress={() => this.getCameraPermissions("scanner")}
+        >
+          <Text style={styles.buttonText}>Escanear código QR</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black"
+  },
+  text: {
+    color: "green",
+    fontSize: 15
+  },
+  button: {
+    width: "43%",
+    height: 55,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "yellow",
+    borderRadius: 15,
+    borderWidth: 2
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "green"
+  }
+});
---
+++ RideHistory.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default class RideHistoryScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Pantalla de historial</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black"
+  },
+  text: {
+    color: "green",
+    fontSize: 30
+  }
+});
